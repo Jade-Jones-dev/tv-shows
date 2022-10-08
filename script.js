8bcad6412d755aab894ce54b5fcd1d50c59fc06f
JadeJones2022 <100792140+JadeJones2022@users.noreply.github.com>
2022-10-08 16:34:19 +0100

    notes for first part of task

--- script.js
+++ script.js
@@ -4,9 +4,34 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
+// for episode of allEpisodes
+
+
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+// what do I need to do?
+
+ // episodeList for each episode
+
+ // create a div
+
+ // episode name
+
+ // episode code must be created with if num is <10
+
+ // episode image - image medium
+
+ // episode summary
+
+ // level 200 create a search input
+
+ // add an episode selector
+
+
+
+
 }
 
 window.onload = setup;
