e31ddb49b1c033c2dbd41f70c4d7cd27ef7941af
JadeJones2022 <100792140+JadeJones2022@users.noreply.github.com>
2022-10-15 20:34:38 +0100

    refactored code so I can read it

--- script.js
+++ script.js
@@ -1,112 +1,73 @@
 //You can edit ALL of the code here
-let allEpisodes = [];
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-}
-
-// for episode of allEpisodes
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
- //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-
-//fixing search element issue
-const searchFunctionEl = document.querySelector(".search-functions")
+// initial variables
+const allEpisodes = getAllEpisodes();
+const rootElem = document.getElementById("root");
+const searchFunctionEl = document.getElementById("search-functions");
 const episodesList = document.getElementById("episodes");
-rootElem.append(searchFunctionEl, episodesList)
+const episodeDropdown = document.getElementById("episodeDropdown");
 
-let inputEl = document.getElementById("searchInput");
-inputEl.addEventListener('input', searchForEpisodes)
-let searchResults = document.createElement('p')
-
-
-let selectElement = document.getElementById("select");
-selectElement.textContent = "";
-
-  //create option element and append
-
-let optionElement = document.createElement("option");
-optionElement.className = "option";
-selectElement.append(optionElement);
+// initial functions
+function setup() {
+	const allEpisodes = getAllEpisodes();
+	makePageForEpisodes(allEpisodes);
   
-  
-
-
-
-//episodeList.forEach((episode) => {select.add(new Option(optionElement = `${episode.name} S${seasonNumber}E${episodeNumber}`))})
-
-searchFunctionEl.append(searchResults)
-
-// need to fix this so it clears after next input
-searchResults.innerHTML = `Got ${episodeList.length} episode(s)`;
-
-// creating initial elements for episodes
-episodeList.forEach(episode => {
-  let cardEl = document.createElement('div');
-  let imageEl = document.createElement("img");
-  let titleEl = document.createElement('h2');
-
-  let summaryEl = document.createElement('div');
-  let seasonNumber
-  let episodeNumber
-
- //adding 0 to episode and season numbers
-
-    if (episode.season < 10) {
-      seasonNumber = `0${episode.season}`
-      }
-      else {
-        seasonNumber = episode.season
-      }
-    
-    if (episode.number < 10) {
-					episodeNumber = `0${episode.number}`;
-			} else {
-					episodeNumber = episode.number;
-			}
-		
-  cardEl.className = 'card';
-  titleEl.className = 'title';
-  imageEl.className = 'img';
-  summaryEl.className = 'summary';
-
-  titleEl.innerText = `${episode.name} S${seasonNumber}E${episodeNumber}`;
-  imageEl.setAttribute("src" , episode.image.medium);
-  summaryEl.innerHTML = episode.summary;
-
-  // use append rather than append child so that you can do multiple elemnts t once
-  cardEl.append(imageEl, titleEl, summaryEl);
-  episodesList.append(cardEl);
-}) 
-
 }
 
-function searchForEpisodes() {
-	const searchInput = document.getElementById("searchInput").value;
-	const allEpisodes = getAllEpisodes();
-	const filteredEpisode = allEpisodes.filter((episode) => {
+function makePageForEpisodes(episodeList) {
+	const rootElem = document.getElementById("root");
+	rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+	episodeList.forEach((episode) => {
+		let cardEl = document.createElement("div");
+		let imageEl = document.createElement("img");
+		let titleEl = document.createElement("h2");
+		let summaryEl = document.createElement("div");
+		let seasonNumber;
+		let episodeNumber;
+
+		//adding 0 to episode and season numbers
+		if (episode.season < 10) {
+			seasonNumber = `0${episode.season}`;
+		} else {
+			seasonNumber = episode.season;
+		}
+
+		if (episode.number < 10) {
+			episodeNumber = `0${episode.number}`;
+		} else {
+			episodeNumber = episode.number;
+		}
+
+		cardEl.className = "card";
+		titleEl.className = "title";
+		imageEl.className = "img";
+		summaryEl.className = "summary";
+    titleEl.innerText = `${episode.name} S${seasonNumber}E${episodeNumber}`;
+		imageEl.setAttribute("src", episode.image.medium);
+		summaryEl.innerHTML = episode.summary;
+
+		cardEl.append(imageEl, titleEl, summaryEl);
+		episodesList.append(cardEl);
+	});  
+}
+
+//adding event listener to search box
+search.addEventListener("keyup", searchShows);
+
+// creating search function
+function searchShows() {
+	let search = document.getElementById("search").value;
+	const filteredEpisodes = allEpisodes.filter((episode) => {
 		return (
-			episode.name.toLowerCase().includes(searchInput.toLowerCase()) ||
-			episode.summary.toLowerCase().includes(searchInput.toLowerCase())
+			episode.name.toLowerCase().includes(search.toLowerCase()) ||
+			episode.summary.toLowerCase().includes(search.toLowerCase())
 		);
 	});
-	const episodeResults = document.getElementById("episodes");
-	episodeResults.innerHTML = "";
-	makePageForEpisodes(filteredEpisode);
-  searchResults.innerHTML = `Got ${episodeList.length} episode(s)`;
+	const results = document.getElementById("episodes");
+	results.innerHTML = "";
+	makePageForEpisodes(filteredEpisodes);
 }
 
-// create the function
-// create an option
-// set the text
-// append the options
-
-
-
-// next steps finding how to do the search function
-// create a dynamic search feature
-
-
+// creating dropdown
 
 
 
