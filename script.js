1305fd1b38b0a56ff99e9d666c9d8a4cac656f51
JadeJones2022 <100792140+JadeJones2022@users.noreply.github.com>
2022-10-18 20:44:15 +0100

    dropdown working and fetching data from api

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 //You can edit ALL of the code here
 // initial variables
-const allEpisodes = getAllEpisodes();
 const rootElem = document.getElementById("root");
 const searchFunctionEl = document.getElementById("search-functions");
 const episodesList = document.getElementById("episodes");
 const episodeDropdown = document.getElementById("episodeDropdown");
+let allEpisodes; 
 let seasonNumber;
 let episodeNumber;
-let url = "https://api.tvmaze.com/shows/82/episodes";
+let url = "https://api.tvmaze.com/shows/76/episodes";
 
-//how do I switch from obj to live data?
+
+fetch(url)
+.then((response) => response.json())//necessary to get json response 
+.then((data) => (allEpisodes = data))//storing the data in the variable
 
 // initial functions
 function setup() {
-	const allEpisodes = getAllEpisodes();
-	makePageForEpisodes(allEpisodes);
-  
+	
+	makePageForEpisodes(allEpisodes);  
 }
 
+// creating the initial cards for episodes
 function makePageForEpisodes(episodeList) {
 	const rootElem = document.getElementById("root");
 	rootElem.textContent = ` ${episodeList.length} episode(s)`;
@@ -25,38 +28,34 @@ function makePageForEpisodes(episodeList) {
 		let cardEl = document.createElement("div");
 		let imageEl = document.createElement("img");
 		let titleEl = document.createElement("h2");
-    let seasonEl = document.createElement("h3")
+    	let seasonEl = document.createElement("h3")
 		let summaryEl = document.createElement("div");
-		//let seasonNumber moved outside so I can use in dropdown;
-		//let episodeNumber moved outside so I can use in dropdwon;
-
 		//adding 0 to episode and season numbers
 		if (episode.season < 10) {
 			seasonNumber = `0${episode.season}`;
 		} else {
 			seasonNumber = episode.season;
 		}
-
 		if (episode.number < 10) {
 			episodeNumber = `0${episode.number}`;
 		} else {
 			episodeNumber = episode.number;
 		}
-
 		cardEl.className = "card";
 		titleEl.className = "title";
-    seasonEl.className = "season";
+    	seasonEl.className = "season";
 		imageEl.className = "img";
 		summaryEl.className = "summary";
-    titleEl.innerText = `${episode.name}`;
-    seasonEl.innerText = `S${seasonNumber}E${episodeNumber}`;
+    	titleEl.innerText = `${episode.name}`;
+    	seasonEl.innerText = `S${seasonNumber}E${episodeNumber}`;
 		imageEl.setAttribute("src", episode.image.original);
 		summaryEl.innerHTML = episode.summary;
-
+		//appending the cards to the the container
 		cardEl.append(imageEl, titleEl, seasonEl, summaryEl);
 		episodesList.append(cardEl);
 	});
-  dropDownMenu(episodeList); 
+	// creating the episode list dropdown menu by calling the function
+  	dropDownMenu(episodeList); 
 }
 
 //adding event listener to search box
@@ -92,26 +91,25 @@ function dropDownMenu(episodeList){
 			episodeNumber = episode.number;
 		}
 		optionEl.innerHTML = `${episode.name} S${seasonNumber}E${episodeNumber} `;
+		optionEl.value = `E${episode.season}S${episode.number}`
 		episodeDropdown.append(optionEl);
 	})
   
 }
 
 
-// next step cerate a an event listener for when the user clicks the dropdown
-//create a function to show the episode that the user picks
+// adding event listener to the dropdown
+episodeDropdown.addEventListener('change', function(e){
 
-// create a fetch for the data - how should i then switch to fetching - google this
-
-
-
-
-
-// next episode selector
-
-
-
-
+	let selectedEpisode = e.target.value
+	let matchedEpisode = allEpisodes.filter((episode) => {
+	return selectedEpisode == `E${episode.season}S${episode.number}`;
+		 }	
+	)
+	const results = document.getElementById("episodes");
+	results.innerHTML = "";
+	makePageForEpisodes(matchedEpisode);
+})
 
 
 window.onload = setup;
