8ddb71d6d58ed099cf7c09ef48370a79418b6e17
JadeJones2022 <100792140+JadeJones2022@users.noreply.github.com>
2022-11-04 08:50:14 +0000

    sorted shows and dropdown working

--- script.js
+++ script.js
@@ -4,70 +4,56 @@ const rootElem = document.getElementById("root");
 const searchFunctionEl = document.getElementById("search-functions");
 const episodesList = document.getElementById("episodes");
 const episodeDropdown = document.getElementById("episodeDropdown");
+const searchDropdownEl = document.getElementById('searchDropdown')
 const showList = document.getElementById("shows");
 let allEpisodes;
 let seasonNumber;
 let episodeNumber;
 let url = "https://api.tvmaze.com/shows/82/episodes";
 let showsUrl = "https://api.tvmaze.com/shows";
-let allShows = getAllShows()
+let allShows = getAllShows();
+let showsList
 
-// fetch(url)
-// 	.then((response) => response.json()) //necessary to get json response
-// 	.then((data) => (allEpisodes = data)); //storing the data in the variable
 
-// initial functions
-function setup() {
 	 fetch(showsUrl)
 			.then((response) => response.json())
 			.then((data) => {
 				allShows = data;
 				getAllShows();
 			});
-			makePageForShows(allShows);
-	 
+		makePageForShows(allShows);
+// initial functions
+function setup() {
+	
+
 }
 
 function makePageForShows(showsList) {
+  		showsList.sort((a, b) => {        let aShow = a.name.toLowerCase();
+    	let bShow = b.name.toLowerCase();
+    	return aShow < bShow ? -1 : 1})
+
 		showsList.forEach((show) => {
 		let cardEl = document.createElement("div");
+		let option = document.createElement('option')
 		let imageEl = document.createElement("img");
 		let titleEl = document.createElement("h2");
+		titleEl.id = show.id;
+		option.className ='option'
 		cardEl.className = "card";
 		titleEl.className = "title";
+		option.innerText =`${show.name}`;
+		option.id = show.id
 		titleEl.innerText = `${show.name}`;
 		imageEl.setAttribute("src", show.image.medium);
 		imageEl.className = "img";
 		cardEl.append(imageEl, titleEl,);
-		showList.append(cardEl);})
+		showList.append(cardEl);
+		searchDropdownEl.append(option)
+	})
+		
 }
-/*
-	let seasonEl = document.createElement("h3");
-		let summaryEl = document.createElement("div");
-		//adding 0 to episode and season numbers
-		if (episode.season < 10) {
-			seasonNumber = `0${episode.season}`;
-		} else {
-			seasonNumber = episode.season;
-		}
-		if (episode.number < 10) {
-			episodeNumber = `0${episode.number}`;
-		} else {
-			episodeNumber = episode.number;
-		}
-		
-		seasonEl.className = "seasons";
-		
-		summaryEl.className = "summary";
-		
-		seasonEl.innerText = `S${seasonNumber}E${episodeNumber}`;
-		
-		summaryEl.innerHTML = show.summary;
-		//appending the cards to the the container
-		
-	});
-	// creating the episode list dropdown menu by calling the function
-	dropDownMenu(episodeList);*/
+
 
 // creating the initial cards for episodes
 function makePageForEpisodes(episodeList) {
@@ -156,27 +142,15 @@ episodeDropdown.addEventListener("change", function (e) {
 	makePageForEpisodes(matchedEpisode);
 });
 
-// create a show selector
-function dropDownMenu2(allShows) {
-	allShows.forEach((show) => {
-		let optionEl = document.createElement("option");
-		optionEl.innerHTML = `${show.name}`;
-		optionEl.value = show.id;
-		searchDropdown.append(optionEl);
-	});
+// 
+function displayTheEpisode(e) {
+	removeAllChildeNodes(rootElem);
+	showId = e.target.value;
+	fetch("https://api.tvmaze.com/shows/" + showId + "/episodes")
+		.then((response) => response.json())
+		.then((data) => (allEpisodes = data))
+		.then(() => getEpisodes());
 }
-
-//sorts the shows into alphabetical order
-
-allShows.sort((a, b) => {
-	if (a.name.toLowerCase() > b.name.toLowerCase()) {
-		return 1;
-	} else if (b.name.toLowerCase() > a.name.toLowerCase()) {
-		return -1;
-	} else {
-		return 0;
-	}
-});
 
 
 /*
